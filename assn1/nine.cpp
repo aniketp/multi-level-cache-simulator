08b0c8f4e302a4600ef740fe875f1d09a4857fae
Aniket Pandey <aniketp@iitk.ac.in>
2019-08-22 10:58:31 +0530

    Add NINE policy source file

---
+++ assn1/nine.cpp
@@ -0,0 +1,29 @@
+#include "cache.h"
+
+int main() {
+    Cache l2(L2Cache, 8, 1024);
+    Cache l3(L3Cache, 16, 1024);
+
+    int type, addr, evicted;
+    int hits, misses;
+
+    if (l2.check_hit_or_miss(addr)) {
+        l2.hits++;
+        l2.update_on_hit(addr);
+    } else {
+        l2.misses++;
+        if (l3.check_hit_or_miss(addr)) {
+            l3.hits++;
+            l3.update_on_hit(addr);
+            evicted = l2.add_block(addr);
+            if (!l3.check_hit_or_miss(evicted))
+                l3.add_block(evicted);
+        } else {
+            l3.misses++;
+            l3.add_block(addr);    
+            evicted = l2.add_block(addr);
+            if (!l3.check_hit_or_miss(evicted))
+                l3.add_block(evicted);
+        }
+    }
+}
